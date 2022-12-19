ec1f393895416ce64f9616bd3246736a2ae03abf
NANO <File changed>
2022-12-19 17:43:14 +0600

    6 submission

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/zhanserikabdu/Tuna";
+    public static String REPO_LINK = "https://github.com/zhanserikabdu/Code";
 }
